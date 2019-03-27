6efe274101097fe300a7118d64c1fd75f0249f9a
saurabh-12 <saurabh.sharma569@gmail.com>
2019-03-27 10:34:29 +0530

    Important Array interview Question

--- src/com/saurabh/data/structures/ArrayQuestion/ArrayQuestion.java
+++ src/com/saurabh/data/structures/ArrayQuestion/ArrayQuestion.java
@@ -1,5 +1,8 @@
 package com.saurabh.data.structures.ArrayQuestion;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class ArrayQuestion {
 	
 	
@@ -121,6 +124,210 @@ public class ArrayQuestion {
 		arr[i] = temp;
 	}
 	
+	// Q. 6. Sort Array 1 to N. It contains unique element from 1 to N. 
+// int arr [] = {8,5,7,9,3,1,4,10,2,6}
+public void  sortArray1toN(int [] arr, int size)
+{
+	int curr, next, value;
+	for(int i = 0; i < size; i++)
+	{
+		curr = i;
+		value = -1;
+		// Swap to move element in proper position
+		while(curr >=0  && curr<size && arr[curr] != curr+1)
+		{
+			next = arr[curr];
+			arr[curr] = value;
+			value = next;
+			curr = next - 1;
+		}
+	}
+}
+
+//  Q. 6. 2nd Method Swapping the element
+// int arr [] = {8,5,7,9,3,1,4,10,2,6}
+public void sortArray1ToN(int arr [], int size)
+{
+	int temp;
+	for(int i = 0 ; i <size; i++)
+	{
+		while(arr[i] != i+1 && arr[i]>1 )
+		{
+			temp = arr[i];
+			arr[i] = arr[temp-1];
+			arr[temp -1]  = temp;
+		}
+	}
+}
+
+// Q.7. Index Array :- Given array of size N, containing elements from 0 to N-1. 
+// All values from 0 to N-1 are present in array and if they are not there than put -1
+// i/p = [8, -1,6,1,9,3,2,7,4,-1]
+//o/p = [-1,1,2,3,4,-1,6,7,8,9]
+public void indexAray(int [] arr, int size)
+{
+	int temp;
+	for(int i = 0; i<size; i++)
+	{
+		while(arr[i] != -1 &&  arr[i] != i)
+		{
+			//Swap
+			temp = arr[i];
+			arr[i] = arr[temp];
+			arr[temp] = temp;
+		}
+	}
+}
+
+// Q. 8. Maximum and Minimum Array for sorted array
+// I/P = { 1,2,3,4,5,6,7}
+//o/p = {7,1,6,2,5,3,4}
+public void MaxMinArray(int arr [], int size)
+{
+	int auxArr[] = new int [size];
+		
+	for (int i = 0; i < size; i++) {
+		auxArr[i] = arr[i];
+	}
+	
+	int start = 0;
+	int stop = size-1;
+	
+	for (int i = 0; i < size; i++) {
+		
+		if (i%2 ==0)
+		{
+			arr[i] = auxArr[stop];
+			stop--;
+		}
+		else {
+			arr[i] = auxArr[start];
+			start++;
+			
+		}
+	}
+}
+
+// Q. 9. Given an unsorted array, find smallest positive number missing  in the array. 
+// i/p = {8,5,6,1,9,11,2, 7, 4,10}
+//o/p = {3}
+public  int smallestPositiveMissingNumber(int [] arr, int size)
+{
+	//O[n^2)
+	int found;
+	for (int i = 1; i < size+1; i++) {
+		found = 0;
+		
+		for (int j = 0; j < size; j++) {
+	
+			if(arr[j] == i)
+			{
+				found = 1;
+				break;
+			}
+		}
+		if(found ==0) 
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+// Second Method  using Hash table
+public int smallestPositiveMissingNumber2(int arr [], int size)
+{
+	HashMap<Integer, Integer>hs = new HashMap<>();
+	
+	for (int i = 0; i < size; i++) {
+		hs.put(arr[i], 1);
+	}
+	
+	for (int i = 1; i <size+1; i++) {
+		if(hs.containsKey(i)==false)
+		{
+			return i;
+		}
+	}
+	
+	return -1;
+}
+//Third Method  =  rearranging the elements. O(n)
+public int smallestPositiveMissingNumber3(int arr[], int size)
+{
+	int temp;
+	for (int i = 0; i <size; i++) {
+
+		while(arr[i] != i+1 && arr[i]>0 && arr[i]<= size)
+		{
+			temp = arr[i];
+			arr[i] = arr[temp-1];
+			arr[temp-1] = temp;
+		}
+	}
+	
+	for(int i = 0; i<size;i++)
+	{
+		if(arr[i]!= i+1)
+		{
+			return i+1;
+		}
+	}
+
+	return -1;
+}
+
+//Fourth Method : Using Auxiliary array
+public int smallestPositiveMissingNumber4(int []arr, int size)
+{
+	int []auxArr = new int[size];
+	Arrays.fill(auxArr, -1);
+	
+	for(int i = 0; i<size; i++)
+	{
+		if(arr[i]>0 && arr[i]<=size)
+		{
+			auxArr[arr[i]-1] = arr[i];
+		}
+	}
+	printArray(auxArr);
+	
+	for(int i = 0; i<size; i++)
+	{
+		if(auxArr[i] != i+1)
+		{
+			return i+1;
+		}
+	}
+	return -1;
+}
+
+//Q. 10.  Array Wave form :- Arrange elements in wave form such that odd element are lesser than its
+// neighbouring even number
+// i/p = {8,1,2,3,4,5,6,4,2}
+//o/p = {}
+
+public void waveArray(int [] arr, int size)
+{
+	//odd elements are lesser then even element
+	for(int i = 1; i <size; i+=2)
+	{
+		if((i-1)>=0 && arr[i]>arr[i-1]) {
+			swap(arr, i, i-1);
+		}
+		if((i+1)<size && arr[i]>arr[i+1])
+		{
+			swap(arr, i, i+1);
+		}
+	}
+}
+
+private void swap(int[] arr, int i, int j) {
+	int temp;
+	temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] =temp;
+}
+
 
 public void printArray(int arr[])
 {
--- src/com/saurabh/data/structures/ArrayQuestion/TestArrayQuestion.java
+++ src/com/saurabh/data/structures/ArrayQuestion/TestArrayQuestion.java
@@ -8,9 +8,39 @@ public class TestArrayQuestion {
 		
 		int arr [] = {10,20,30,40,50,60};
 		arrQuestionObj.printArray(arr);
-		
-		arrQuestionObj.rotateArrayByKPos(arr, 6, 2);
+		arrQuestionObj.rotateArrayByKPos(arr, arr.length, 2);
 		arrQuestionObj.printArray(arr);
+		
+		System.out.println("-------------------------------------");
+		int arr2 [] = {8,5,7,9,3,1,4,10,2,6};
+		arrQuestionObj.printArray(arr2);
+		arrQuestionObj.sortArray1toN(arr2, arr2.length);
+		arrQuestionObj.printArray(arr2);
+		
+		System.out.println("-------------------------------------");
+		int arr3 [] = {8, -1,6,1,9,3,2,7,4,-1};
+		arrQuestionObj.printArray(arr3);
+		arrQuestionObj.indexAray(arr3, arr3.length);
+		arrQuestionObj.printArray(arr3);
+		
+		System.out.println("-------------------------------------");
+		int arr4 [] = { 1,2,3,4,5,6,7};
+		arrQuestionObj.printArray(arr4);
+		arrQuestionObj.MaxMinArray(arr4, arr4.length);
+		arrQuestionObj.printArray(arr4);
+		
+		System.out.println("-------------------------------------");
+		int arr5 [] = {8,5,6,1,9,11,2, 7, 4,10};
+		arrQuestionObj.printArray(arr5);
+		int missingValue = arrQuestionObj.smallestPositiveMissingNumber4(arr5, arr5.length);
+		System.out.println("missingValue "+missingValue);
+		arrQuestionObj.printArray(arr5);
+		
+		System.out.println("-------------------------------------");
+		int arr6 [] = {8,1,2,3,4,5,6,4,2};
+		arrQuestionObj.printArray(arr6);
+		arrQuestionObj.waveArray(arr6, arr6.length);
+		arrQuestionObj.printArray(arr6);
 		
 		
 	}
