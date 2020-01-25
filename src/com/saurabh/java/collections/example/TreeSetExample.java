c79561d3bb5fbe5b310ae4be215e0fb121a36ca9
Saurabh-12 <saurabh.sharma569@gmail.com>
2020-01-25 18:15:05 +0530

    Java Collection TreeSetExample

---
+++ src/com/saurabh/java/collections/example/TreeSetExample.java
@@ -0,0 +1,18 @@
+package com.saurabh.java.collections.example;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+	public static void main(String[] args) {
+		
+		TreeSet<String>ts = new TreeSet<String>();
+		ts.add("India");
+		ts.add("USA");
+		ts.add("Brazil");
+		
+		System.out.println("Content of TreeSet: "+ts);
+		
+		
+	}
+}
