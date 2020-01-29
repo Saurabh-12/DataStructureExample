d838e742ed0745c8fdba28754c3d28fd44503409
Saurabh-12 <saurabh.sharma569@gmail.com>
2020-01-29 22:14:34 +0530

    Bubblesort Example

--- src/com/saurabh/algo/sorting/BubbleSortExample.java
+++ src/com/saurabh/algo/sorting/BubbleSortExample.java
@@ -28,11 +28,10 @@ public class BubbleSortExample {
         int temp, swap = 1;
 
         for (int i = 0; i <(size-1) && swap ==1; i++) {
-            System.out.println("outer Loop: "+i);
             
             for (int j = 0; j < (size-i-1); j++) {
                 swap = 0;
-                System.out.println("Inner Loop: "+j);
+                
                 if(more(arr[j], arr[j+1])){
                     temp = arr[j];
                     arr[j] = arr[j+1];
@@ -40,7 +39,6 @@ public class BubbleSortExample {
                     swap =1;
                 }                
             }
-
         }
     }
 
