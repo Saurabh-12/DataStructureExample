7e717cf2750ab72468cbacf693a26586169f3d54
Saurabh-12 <saurabh.sharma569@gmail.com>
2020-02-27 08:56:29 +0530

    Bucket Sort Examnple

---
+++ src/com/saurabh/algo/sorting/BucketSortExample.java
@@ -0,0 +1,35 @@
+package com.saurabh.algo.sorting;
+
+public class BucketSortExample {
+
+
+
+    public static void bucketSort(int [] arr, int lowerRange, int upperRange)
+    {
+        int i, j;
+        int size = arr.length;
+        int range = upperRange - lowerRange;
+        int count[] = new int[range];
+
+        for(i = 0; i<size;i++){
+            count[arr[i]-lowerRange]++;
+        }
+
+        j=0;
+        for(i=0;i<range;i++){
+            for(;count[i]>0;(count[i])--){
+                arr[j++] = i+lowerRange;
+            }
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        int [] array = {23,24,22,21,26,25,27,28,21,21};
+        bucketSort(array, 20, 30);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+        }
+        System.out.println(" ");
+    }
+}
