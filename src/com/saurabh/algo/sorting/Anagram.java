52194f1c7b1ef90422e93aa6aa5b09a834e53873
Saurabh Sharma <saurabh.sharma569@gmail.com>
2022-12-29 20:44:48 +0530

    White space removal

--- src/com/saurabh/algo/sorting/Anagram.java
+++ src/com/saurabh/algo/sorting/Anagram.java
@@ -1,57 +1,45 @@
-package com.saurabh.algo.sorting;
+packagecom.saurabh.algo.sorting;
+publicclassAnagram{
 
-public class Anagram {
-    
-    public static int size;
-    public static char [] arrChar = new char[100];
-
-    public static void main(String[] args) {
-        //System.out.println("ddddddd");
-        String cat = "cat";
-        size = cat.length();
-        for (int i = 0; i < size; i++) {
-            arrChar[i] = cat.charAt(i);
-        }
-
-        doAnagram(size);
-        //displayAnagram();
-    }
-
-    public static void doAnagram(int newSize) {
-
-        if(newSize ==1)
-            return;
-
-        for (int i = 0; i < newSize; i++) {
-            doAnagram(newSize - 1);
-            if(newSize == 2)
-                displayAnagram();
-          rotateAnagram(newSize);
-        }
-    }
-
-    private static void displayAnagram() {
-        System.out.println("      ");
-        for (int i = 0; i < size; i++) {
-            System.out.print(""+arrChar[i]+"");
-        }
-        System.out.println("        ");
-    }
-
-    private static void rotateAnagram(int newSize) {
-        int j;
-        int pos = size - newSize;
-        char temp = arrChar[pos];
-
-        for (j = pos+1; j < size; j++) {
-            arrChar[j-1] = arrChar[j];
-            //arrChar[j-1] = temp;
-        }
-
-        arrChar[j-1] = temp;
-    }
+publicstaticintsize;
+publicstaticchar[]arrChar=newchar[100];
+publicstaticvoidmain(String[]args){
+//System.out.println("ddddddd");
+Stringcat="cat";
+size=cat.length();
+for(inti=0;i<size;i++){
+arrChar[i]=cat.charAt(i);
+}
+doAnagram(size);
+//displayAnagram();
+}
+publicstaticvoiddoAnagram(intnewSize){
+if(newSize==1)
+return;
+for(inti=0;i<newSize;i++){
+doAnagram(newSize-1);
+if(newSize==2)
+displayAnagram();
+rotateAnagram(newSize);
+}
+}
+privatestaticvoiddisplayAnagram(){
+System.out.println("");
+for(inti=0;i<size;i++){
+System.out.print(""+arrChar[i]+"");
+}
+System.out.println("");
+}
+privatestaticvoidrotateAnagram(intnewSize){
+intj;
+intpos=size-newSize;
+chartemp=arrChar[pos];
+for(j=pos+1;j<size;j++){
+arrChar[j-1]=arrChar[j];
+//arrChar[j-1]=temp;
+}
+arrChar[j-1]=temp;
+}
 
 
-    
-    
 }
